7a3f9c2c3fc6d13618000509bdb9cb6e36fe5d00
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-13 18:13:30 +0100

    feat: create a function getAstros

--- chapter4/get-json/main.go
+++ chapter4/get-json/main.go
@@ -13,13 +13,14 @@ type people struct {
 	Number int `json:"number"`
 }
 
-func main() {
+func getAstros(apiURL string) (people, error) {
 
-	url := "http://api.open-notify.org/astros.json"
+	p := people{}
+
 	spaceClient := http.Client{
 		Timeout: time.Second * 2, // Timeout after 2 seconds
 	}
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, apiURL, nil)
 
 	req.Header.Set("User-Agent", "spacecount-tutorial")
 	res, err := spaceClient.Do(req)
@@ -34,10 +35,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	p := people{}
 	err = json.Unmarshal(body, &p)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(p.Number)
+	return p, nil
+
+}
+func main() {
+
+	apiURL := "http://api.open-notify.org/astros.json"
+	people, err := getAstros(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d people found in space.\n", people.Number)
+
 }
