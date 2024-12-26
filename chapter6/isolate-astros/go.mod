c91ea6673032c5db5d654a5cbd5b951bf0fc211d
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-26 20:09:39 +0100

    feat(chapter6): implementation of http request get astros

---
+++ chapter6/isolate-astros/go.mod
@@ -0,0 +1,3 @@
+module github.com/vramahandry/everyday-go/chapter6/isolate_astros
+
+go 1.23.4
---
+++ chapter6/isolate-astros/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/vramahandry/everyday-go/chapter6/isolate_astros/astros"
+)
+
+func getAstronauts(getWebRequest astros.GetWebRequest) (int, error) {
+	url := "http://api.open-notify.org/astros.json"
+	body, err := getWebRequest.FetchBytes(url)
+	if err != nil {
+		return 0, err
+	}
+	peopleResult := astros.People{}
+	if err := json.Unmarshal(body, &peopleResult); err != nil {
+		return 0, err
+	}
+	return peopleResult.Number, err
+}
+
+func main() {
+	liveClient := astros.LiveGetWebRequest{}
+	number, err := getAstronauts(liveClient)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(number)
+}
