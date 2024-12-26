2efa44d7fc95dd02a8c1bbcdd3368b09b6fab7bc
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-26 18:39:14 +0100

    feat: wrap test in one func with list struct

--- chapter6/sum-testing/main_test.go
+++ chapter6/sum-testing/main_test.go
@@ -2,23 +2,22 @@ package main
 
 import "testing"
 
-func Test_sum_odd_numbers(t *testing.T) {
-	x := 5
-	y := 5
-	want := 10
-	got := sum(x, y)
-	t.Logf("Testing a sum of: %d %d", x, y)
-	if want != got {
-		t.Errorf("Sum was incorrect want: %d, but got: %d", want, got)
+func Test_sum(t *testing.T) {
+	tables := []struct {
+		x    int
+		y    int
+		want int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
 	}
-}
-func Test_sum_negative_numbers(t *testing.T) {
-	x := -5
-	y := -5
-	want := -10
-	got := sum(x, y)
-	t.Logf("Testing a sum of: %d %d", x, y)
-	if want != got {
-		t.Errorf("Sum was incorrect want: %d, but got: %d", want, got)
+	for _, table := range tables {
+		got := sum(table.x, table.y)
+		if got != table.want {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.",
+				table.x, table.y, got, table.want)
+		}
 	}
 }
