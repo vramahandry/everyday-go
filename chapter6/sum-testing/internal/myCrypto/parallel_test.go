ff527ff92bdaa7c8b8a3156403adfdc5cdc32089
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-26 19:06:37 +0100

    feat: parallel test

---
+++ chapter6/sum-testing/internal/myCrypto/parallel_test.go
@@ -0,0 +1,27 @@
+package myCrypto
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_Slow1(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+func Test_Slow2(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+func Test_Slow3(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+func Test_Slow4(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+func Test_Slow5(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
