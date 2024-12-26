8cc27869cbe4f8d6037e0f847d2c912e8d479eae
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-26 18:25:26 +0100

    feat(chapter6): func sum

---
+++ chapter6/sum-testing/go.mod
@@ -0,0 +1,3 @@
+module github.com/vramahandry/everyday-go/chapter6/sum-testing
+
+go 1.23.4
---
+++ chapter6/sum-testing/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func sum(x int, y int) int {
+	// This code has a bug that adds 1
+	return x + y + 1
+}
+func main() {
+	x := 5
+	y := 5
+	fmt.Printf("%d + %d = %d\n", x, y, sum(x, y))
+}
