d938f56c7ca35566aac253cebe379989970d97c4
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-26 18:29:02 +0100

    feat: add negative numbers test

--- chapter6/sum-testing/main_test.go
+++ chapter6/sum-testing/main_test.go
@@ -2,14 +2,23 @@ package main
 
 import "testing"
 
-func Test_sum(t *testing.T) {
+func Test_sum_odd_numbers(t *testing.T) {
 	x := 5
 	y := 5
 	want := 10
 	got := sum(x, y)
+	t.Logf("Testing a sum of: %d %d", x, y)
 	if want != got {
-		t.Logf("Testing a sum of: %d %d", x, y)
 		t.Errorf("Sum was incorrect want: %d, but got: %d", want, got)
-
+	}
+}
+func Test_sum_negative_numbers(t *testing.T) {
+	x := -5
+	y := -5
+	want := -10
+	got := sum(x, y)
+	t.Logf("Testing a sum of: %d %d", x, y)
+	if want != got {
+		t.Errorf("Sum was incorrect want: %d, but got: %d", want, got)
 	}
 }
