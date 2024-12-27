59465fc70221928ea39f2548326bac469b61d04d
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-27 17:37:01 +0100

    fake http server

--- chapter6/isolate-astros/main_test.go
+++ chapter6/isolate-astros/main_test.go
@@ -1,16 +1,52 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 type testWebRequest struct {
 }
 
+// type MockHttpClient struct {
+// 	MockDo func(req *http.Request) (*http.Response, error)
+// }
+
+// func (c *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
+// 	return c.MockDo(req)
+// }
+
 func (testWebRequest) FetchBytes(url string) ([]byte, error) {
-	return []byte(`{"number": 2}`), nil
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"number": 2}`))
+	}))
+	defer mockServer.Close()
+
+	req, err := http.NewRequest(http.MethodGet, mockServer.URL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	req.Header.Set("User-Agent", "test")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
 }
 
 func TestGetAstronauts(t *testing.T) {
-	want := 2
+	want := 3
 	got, err := getAstronauts(testWebRequest{})
 	if err != nil {
 		t.Fatal(err)
