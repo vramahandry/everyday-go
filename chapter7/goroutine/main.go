523b4120ba1d78a012b4ccb78b4eb7a29757ca98
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-28 17:35:31 +0100

    feat(chapter7): for goroutines with waitGroup

--- chapter7/goroutine/main.go
+++ chapter7/goroutine/main.go
@@ -2,28 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"sync"
 	"time"
 )
 
 func main() {
 	wg := sync.WaitGroup{}
-	wg.Add(3)
-	go func() {
-		defer wg.Done()
-		printLater("Hello\n", time.Millisecond*100)
-	}()
-	go func() {
-		defer wg.Done()
-		printLater("World\n", time.Millisecond*100)
-	}()
-	go func() {
-		defer wg.Done()
-		printLater(os.Getenv("USER")+"\n", time.Millisecond*100)
-	}()
+	workItems := 5
+	wg.Add(workItems)
+	for i := 1; i <= workItems; i++ {
+		j := i
+		go func() {
+			defer wg.Done()
+			printLater(
+				fmt.Sprintf("Hello from %d\n", j),
+				time.Millisecond*100)
+		}()
+	}
 	wg.Wait()
 }
+
 func printLater(msg string, duration time.Duration) {
 	time.Sleep(duration)
 
