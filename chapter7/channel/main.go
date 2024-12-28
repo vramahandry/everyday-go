065fd630f48ca23eeec4a645150304ef9b04d5a3
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-28 19:09:59 +0100

    feat(chapter7:channel): specify max goroutines concurrent

--- chapter7/channel/main.go
+++ chapter7/channel/main.go
@@ -4,54 +4,97 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
 func main() {
-	var uri string
-	var timeout time.Duration
-	flag.StringVar(&uri, "uri", "", "uri to fetch")
-	flag.DurationVar(&timeout, "timeout", time.Second*5, "timeout for HTTP client")
+	uris := []string{"https://github.com/alexellis",
+		"https://github.com/openfaas",
+		"https://github.com/inlets",
+		"https://github.com/docker",
+		"https://github.com/kubernetes",
+		"https://github.com/moby",
+		"https://github.com/hashicorp",
+		"https://github.com/civo",
+	}
+	var w int
+	flag.IntVar(&w, "w", 1, "amount of workers")
 	flag.Parse()
-	titleCh := make(chan string)
-	errorCh := make(chan error)
-	go titleOf(uri, timeout, titleCh, errorCh)
-	select {
-	case title := <-titleCh:
-		fmt.Printf("Title: %s\n", title)
-	case err := <-errorCh:
-		fmt.Printf("Error: %s\n", err.Error())
+	titleMap, errs := fetch(uris, w)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Fprintf(os.Stderr, "error %s", err.Error())
+		}
 		os.Exit(1)
+		for k, v := range titleMap {
+			fmt.Printf("%s has %s\n", k, v)
+		}
 	}
 }
 
-func titleOf(uri string, timeout time.Duration, titleCh chan string, errCh chan error) {
+func fetch(uris []string, workers int) (map[string]string, []error) {
+	titles := make(map[string]string)
+	var errs []error
+	workQueue := make(chan string)
+	wg := sync.WaitGroup{}
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		worker := i
+		go func(worker int, workQueue chan string) {
+			log.Printf("[Worker: %d] Started work.", worker)
+			for uri := range workQueue {
+				log.Printf("[Worker: %d] Getting: %s", worker, uri)
+				start := time.Now()
+				title, err := titleOf(uri, time.Second*5)
+				if err != nil {
+					errs = append(errs, err)
+				}
+				log.Printf("[Worker: %d] Got: %s (%.2fs)",
+					worker,
+					uri,
+					time.Since(start).Seconds())
+				titles[uri] = title
+			}
+			wg.Done()
+		}(worker, workQueue)
+		log.Printf("[Worker: %d] Finished work.", worker)
+
+	}
+	go func() {
+		for _, u := range uris {
+			workQueue <- u
+		}
+		close(workQueue)
+	}()
+	wg.Wait()
+	return titles, errs
+}
+
+func titleOf(uri string, timeout time.Duration) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
 	if err != nil {
-		errCh <- err
-		return
+		return "", err
 	}
 	res, err := http.DefaultClient.Do(req)
-
 	if err != nil {
-		errCh <- err
-		return
+		return "", err
 	}
 	if res.Body != nil {
 		defer res.Body.Close()
-		res, _ := io.ReadAll(res.Body)
+		res, _ := ioutil.ReadAll(res.Body)
 		for _, l := range strings.Split(string(res), "\n") {
 			if strings.Contains(l, "<title>") {
-				titleCh <- l
-				return
+				return uri, nil
 			}
 		}
 	}
-	errCh <- fmt.Errorf("no title found")
+	return "", fmt.Errorf("no title found")
 }
